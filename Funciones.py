4350f63a5f1a2e0701fee30f73a52a412805a492
marianoefernandez <marianofernandez@MacBook-Air-de-Mariano.local>
2025-05-06 21:01:57 -0300

    Version 1

---
+++ Funciones.py
@@ -0,0 +1,62 @@
+import random
+
+def mostrar_usuarios(lista_nombres:list,lista_edades:list,lista_legajos:list,lista_sueldos:list) -> None:
+    for i in range(len(lista_nombres)):
+        print(f"MUESTRO USUARIO {i + 1}")
+        mostrar_usuario(lista_nombres,lista_edades,lista_legajos,lista_sueldos,i)
+        
+def cargar_usuarios(lista_nombres:list,lista_edades:list,lista_legajos:list,lista_sueldos:list) -> bool:
+    retorno = False 
+    
+    if type(lista_edades) == list and type(lista_nombres) == list and type(lista_legajos) == list and type(lista_sueldos) == list:
+        for i in range(len(lista_nombres)):
+            print(f"CARGANDO EL USUARIO {i + 1}")
+            #Recuerden validar los mismos
+            nombre_usuario = input("Ingrese el nombre: ")
+            edad_usuario = int(input("Ingrese la edad: "))
+            sueldo_usuario = float(input("Ingrese el sueldo: "))
+            legajo_usuario = random.randint(11111,99999)
+            
+            lista_nombres[i] = nombre_usuario
+            lista_legajos[i] = legajo_usuario
+            lista_sueldos[i] = sueldo_usuario
+            lista_edades[i] = edad_usuario
+            
+            #Si quieren mejorar la carga, podrian pedir una confirmacion al usuario en caso de que haya cargado algo mal
+            print("USUARIO CARGADO CON EXITO")
+            retorno = True
+    return retorno
+
+def sumar_sueldos(lista_sueldos:list) -> float:
+    suma_sueldos = 0
+    
+    for i in range(len(lista_sueldos)):
+        suma_sueldos += lista_sueldos[i]
+    
+    return suma_sueldos
+
+def calcular_promedio(acumulador:float,contador:int) -> float | None:
+    if contador != 0:
+        #Puedo calcular el promedio
+        promedio = acumulador / contador
+    else:
+        promedio = None
+        
+    return promedio
+
+def buscar_usuario_mayor_salario(lista_sueldos:list) -> int:
+    maximo = lista_sueldos[0]
+    for i in range(len(lista_sueldos)):
+        if lista_sueldos[i] > maximo:
+            maximo = lista_sueldos[i]
+            indice_usuario = i
+                
+    return indice_usuario
+
+def mostrar_usuario(lista_nombres:list,lista_edades:list,lista_legajos:list,lista_sueldos:list,indice:int) -> None:
+    print(f"Legajo: {lista_legajos[indice]}")
+    print(f"Nombre: {lista_nombres[indice]}")
+    print(f"Edad: {lista_edades[indice]} años")
+    print(f"Sueldo: $ {lista_sueldos[indice]}")
+    print("")
+    
---
+++ Main.py
@@ -0,0 +1,62 @@
+#Los arreglos paralelos (listas paralelas) son arrays que guardan un conjunto de información especifico para un fin por ejemplo guardar los datos de los usuarios, vamos a usar cada array por cada dato del usuario y estos mismos se van a relacionar porque tienen el mismo indice.
+#Los arrays paralelos tienen que tener la misma cantidad de elementos
+
+#Legajo
+#Nombre
+#Edad
+#Sueldo
+
+from Funciones import *
+
+#Tener 3 usuarios en mi sistema
+lista_nombres = [0,0,0]
+lista_legajos = [0,0,0]
+lista_edades = [0,0,0]
+lista_sueldos = [0,0,0]
+bandera_carga = False
+
+#La informacion la relacionamos mediante el indice
+
+#Mostrar la informacion
+
+#Menú
+
+import os
+
+while True:
+    print("1.Cargar Usuarios\n2.Mostrar Usuarios\n3.Mostrar promedio sueldos\n4.Mostrar el empleado con el salario más alto (SOLO UNO)\n5.Salir")
+    opcion = int(input("Su opcion: "))
+    while opcion > 5 or opcion < 1:
+        opcion = int(input("Reingrese su opcion (1-5): "))
+    
+    if opcion == 1:
+        respuesta = cargar_usuarios(lista_nombres,lista_edades,lista_legajos,lista_sueldos)
+        if respuesta == True:
+            print("PUDO CARGAR LOS USUARIOS")
+            bandera_carga = True
+        else:
+            print("NO PUDO CARGAR LOS USUARIOS")
+    elif opcion == 2:
+        if bandera_carga == True:
+            mostrar_usuarios(lista_nombres,lista_edades,lista_legajos,lista_sueldos)
+        else:
+            print("No hay usuarios cargados, no se puede mostrar los mismos")
+    elif opcion == 3:
+        if bandera_carga == True:
+            suma_sueldos = sumar_sueldos(lista_sueldos)
+            promedio_sueldo = calcular_promedio(suma_sueldos,len(lista_sueldos))
+            print(f"El promedio de sueldos es : $ {promedio_sueldo}")
+        else:
+            print("No hay usuarios cargados, no se puede calcular el promedio")
+    elif opcion == 4:
+        if bandera_carga == True:
+            print("USUARIO CON MAYOR SUELDO:")
+            indice_usuario = buscar_usuario_mayor_salario(lista_sueldos)
+            mostrar_usuario(lista_nombres,lista_edades,lista_legajos,lista_sueldos,indice_usuario)
+        else:
+            print("No hay usuarios cargados, no se puede calcular el mayor salario")
+    elif opcion == 5:
+        print("SALIENDO...")
+        break
+    input("Toque cualquier boton para continuar...")
+    os.system("clear")
